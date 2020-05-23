a1152daaa63df5769598923b60f9b1c2d2fa3c27
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-23 07:34:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,7 +3,7 @@ VALUES ("Mr. Chocolate", 2, "M", "brown","upset", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Rowdy", 4, "M", "white","calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Tabitha", 6, "F", "black","distress", 1);
+VALUES ("Tabitha", 6, "F", "black","goofy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Sergeant Brown", 3, "M",  "brown","happy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive)
